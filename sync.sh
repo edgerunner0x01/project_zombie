a4c6f5f45aaa684603163f1d088d84f60c118a83
edgerunner0x01 <147637952+edgerunner0x01@users.noreply.github.com>
2024-02-27 13:01:19 -0600

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,20 +1,14 @@
 main(){
-eliminate=" <loc>https://ba9chich.com/register/</loc>
-            <loc>https://ba9chich.com/about-us/</loc>
-            <loc>https://ba9chich.com/privacy-policies/</loc>
-            <loc>https://ba9chich.com/contact/</loc>
-            <loc>https://ba9chich.com/terms-of-use/</loc>
-            <loc>https://ba9chich.com/creator-verification-request/</loc>
+eliminate=" <loc>https://ba9chich.com/register/</loc>|\
+            <loc>https://ba9chich.com/about-us/</loc>|\
+            <loc>https://ba9chich.com/privacy-policies/</loc>|\
+            <loc>https://ba9chich.com/contact/</loc>|\
+            <loc>https://ba9chich.com/terms-of-use/</loc>|\
+            <loc>https://ba9chich.com/creator-verification-request/</loc>|\
             <loc>https://ba9chich.com/know-your-customer/</loc>" 
             
-eliminate2=" register
-            about-us
-            privacy-policies
-            contact
-            terms-of-use
-            creator-verification-request
-            know-your-customer
 
-curl 'https://www.ba9chich.com/sitemap.xml' -H "Accept: xml,*/*" -H "Accept-Language: en-US,en" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" 2> /dev/null |grep -v "$eliminate" |grep "<loc>" |grep -v post |grep -v creators |sed 's/<loc>//g' |sed 's/<\/loc>//g' |sed 's/https:\/\/ba9chich.com\///g' |sed 's/\///g' |grep -v "$eliminat2"  
+
+curl 'https://www.ba9chich.com/sitemap.xml' -H "Accept: xml,*/*" -H "Accept-Language: en-US,en" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" 2> /dev/null |egrep -v "$eliminate" |grep "<loc>" |grep -v post |grep -v creators |sed 's/<loc>//g' |sed 's/<\/loc>//g' |sed 's/https:\/\/ba9chich.com\///g' |sed 's/\///g'  
 }
 main
